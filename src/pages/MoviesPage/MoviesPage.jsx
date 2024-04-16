9e4e098d7b7593c366834bcd472a8575e2261662
Dobrota <dobrota.art@gmail.com>
2024-04-16 23:05:03 +0300

    update

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,39 +1,36 @@
 import { useState, useEffect } from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { searchMovie } from '../../Api/tmdbApi';
 import MovieList from '../../components/MovieList/MovieList';
 import css from './MoviesPage.module.css';
 
-
-
 const MoviesPage = () => {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
-  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  const queryFromURL = searchParams.get('query');
 
+ 
   useEffect(() => {
+    const queryFromURL = searchParams.get('query');
     if (queryFromURL) {
       (async () => {
         const data = await searchMovie(queryFromURL);
         setMovies(data.results);
       })();
     }
-  }, [queryFromURL]);
+  }, [searchParams]);
 
   const handleSearch = async (event) => {
     event.preventDefault();
     if (!query) return;
-    const data = await searchMovie(query);
-    setMovies(data.results);
-    navigate(`/movies?query=${encodeURIComponent(query)}`);
+    setSearchParams({ query }); 
   };
 
   return (
     <div className={css.form}>
       <form className={css['form-cont']} onSubmit={handleSearch}>
-        <input className={css['inp-cont']}
+        <input
+          className={css['inp-cont']}
           type="text"
           placeholder="Search movies..."
           value={query}
